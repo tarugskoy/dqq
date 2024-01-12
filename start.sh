9caa4e918b5c1172b895e42d9da782db6114c4da
tarugskoy <155990077+tarugskoy@users.noreply.github.com>
2024-01-12 20:35:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://tarugskoy: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@github.com/tarugskoy/dqq
+  git clone https://github.com/tarugskoy/dqq
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ-The-File-Donor
